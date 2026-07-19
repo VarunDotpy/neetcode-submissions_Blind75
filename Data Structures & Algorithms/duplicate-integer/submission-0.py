76e26bdc43c8e13449b6871224f36e427ae34ad2
Varun Srinivasan <srivarun@umich.edu>
2026-07-19 02:51:32 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for value in self.nums():
+            if value in self.num:
+                return True
+            else:
+                return False
+        
